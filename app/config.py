06f4c33d0652524438e711561a08241fce7a703d
mahmoudodoo <mahmoudodeh332@gmail.com.com>
2021-08-30 08:51:12 +0300

    Change DB url2

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
-    SQLALCHEMY_DATABASE_URI ='postgresql://ieddwzefyn:V8353B58L65T5B23$@melearning-server.database.windows.net/postgres'
+    SQLALCHEMY_DATABASE_URI ='postgresql://ieddwzefyn:V8353B58L65T5B23$@melearning-server.postgres.database.azure.com/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS= False
     POSTS_PER_PAGE = 25
     STATIC_PATH = os.path.join(basedir,'static')
