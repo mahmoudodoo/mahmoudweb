56a181d42d8155c16555befd6b021a101f50bf5c
mahmoudodoo <mahmoudodeh332@gmail.com.com>
2021-08-30 12:23:36 +0300

    channge server name

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
-    SQLALCHEMY_DATABASE_URI ='postgresql://ieddwzefyn:V8353B58L65T5B23$@melearning-server.postgres.database.azure.com:5432/postgres'
+    SQLALCHEMY_DATABASE_URI ='postgresql://ieddwzefyn:V8353B58L65T5B23$@melearning-server:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS= False
     POSTS_PER_PAGE = 25
     STATIC_PATH = os.path.join(basedir,'static')
